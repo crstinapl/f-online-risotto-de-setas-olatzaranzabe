6fc52e9952d759e23a389f374754b31a44de0fa4
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-10-30 13:26:45 +0100

    do fetch well

--- main.js
+++ main.js
@@ -2,4 +2,20 @@
 
 const itemsList = document.querySelector('.ingredients__list');
 
-console.log(itemList);
+// console.log(itemList);
+const url = 'https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json';
+
+
+
+function fetchRecipe() {
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson);
+            console.log(myJson.recipe.name)
+        });
+}
+
+window.onload = fetchRecipe;
